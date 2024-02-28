fc352b50a8762c0f843b0f29262061c0d161393b
asianParadissseee <asian.paradissseee@gmail.com>
2024-02-29 01:59:20 +0600

    create table

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,13 @@ async function getUsers() {
     const sheets = document.getElementById("sheets");
 
     data.forEach((user) => {
-        const sheet = document.createElement("div");
+        const sheet = document.createElement("tr");
         sheet.innerHTML = `
-            <div>
-                ${user.name} 
-            </div>
-        `;
+                <td class="">${user.id}</td>
+                <td class="">${user.name}</td>
+                <td class="">${user.email}</td>
+                <td class="">${user.username}</td>
+        `
         sheets.appendChild(sheet);
     });
 }
